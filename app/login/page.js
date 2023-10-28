3846d0f76d7cc60d2b7a8138246827254d66afad
Jorge-AmezcuaC <amezcua.carrillo.jorge.alejandro@gmail.com>
2023-10-27 23:01:09 -0600

    adding login/logup desings, variable primary colors added, and administration title font size bug fixed

--- app/login/page.js
+++ app/login/page.js
@@ -1,9 +1,25 @@
 import Link from "next/link"
+import './login.css'
+
 export default function Login() {
     return (
-      <div>
-        <button><Link href={'/inicio'}>Login</Link></button>
-        <button><Link href={'/logup'}>Registrarse</Link></button>
+      <div className="login-page">
+        <div className="login-container">
+          <div className="login-left">
+            <h3 className="login-left-title">Welcome Back!</h3>
+          </div>
+          <div className="login-rigth">
+            <h3 className="login-rigth-title">Login</h3>
+            <div className="login-input-group">
+              <input type="text" placeholder="Username" className="login-input"/>
+              <input type="password" placeholder="Password" className="login-input"/>
+            </div>
+            <div className="login-button-group">
+              <Link href={'/inicio'} className="login-links"><button className="login-button">Login</button></Link>
+              <Link href={'/logup'} className="login-tologup">Don't have an account? Sign Up!</Link>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
--- app/logup/page.js
+++ app/logup/page.js
@@ -1,8 +1,26 @@
 import Link from "next/link"
+import './logup.css'
+
 export default function LogUp() {
     return (
-      <div>
-        <button><Link href={'login'}>Login</Link></button>
+      <div className="logup-page">
+        <div className="logup-container">
+          <div className="logup-left">
+            <h3 className="logup-left-title">Welcome to DisfrazData</h3>
+          </div>
+          <div className="logup-rigth">
+            <h3 className="logup-rigth-title">Create your account</h3>
+            <div className="logup-input-group">
+              <input type="text" placeholder="Email" className="logup-input"/>
+              <input type="password" placeholder="Password" className="logup-input"/>
+              <div className="logup-options">
+                <p className="logup-text">Got an account?</p>
+                <Link href={'login'} className="logup-tologin">Log in here {'->'}</Link>
+              </div>
+            </div>
+              <Link href={'login'} className="logup-link"><button className="logup-button">SignUp</button></Link>
+          </div>
+        </div>
       </div>
     )
   }
